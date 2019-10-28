6ee20bb664ab09f1359899a4c71d39d6bcfadb08
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-28 16:29:20 -0500

    added my data and also created a constructor function with the super() and this.state as well as imported and put my todolist in a div in the return

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 
+
+
+
+///adding my data like the readme says
+const data = [
+  {
+    task: 'Do the dishes',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Take Catherine on a date!',
+    id: 2,
+    completed: false
+  },
+  {
+    task: 'Make credit card payment',
+    id: 3,
+    completed: true
+  }
+]
+
 class App extends React.Component {
+  //adding constructor and super and state
+  constructor(){
+    super();
+    this.state = {
+      todo: data
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="container-div">
         <h2>Welcome to your Todo App!</h2>
+        <TodoList
+          task={this.state.todo} />
       </div>
     );
   }
